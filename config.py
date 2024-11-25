25e86965c37da7eee6217fa3cb95667d97be7b18
Stoupy51 <stoupy51@gmail.com>
2024-11-25 08:34:27 +0100

    feat: ✨ Added Python Datapack CD scripts

--- config.py
+++ config.py
@@ -7,15 +7,14 @@ IGNORE_UNSET: bool = True							# If True, the program will ignore unset optionn
 # Folders
 BUILD_FOLDER: str = f"{ROOT}/build"					# Folder where the final datapack and resource pack are built
 ASSETS_FOLDER: str = f"{ROOT}/assets"				# Folder where the assets are located
+BUILD_COPY_DESTINATIONS: tuple[list, list] = (["D:/latest_snapshot/world/datapacks"], ["D:/minecraft/snapshot/resourcepacks"])	# Can be empty lists if you don't want to copy the generated files to other folders.
 
 # Datapack related constants
 AUTHOR: str = "Stoupy51"					# Author(s) name(s) displayed in pack.mcmeta, also used to add convention.debug tag to the players of the same name(s) <-- showing additionnal displays like datapack loading
-DATAPACK_NAME: str = "Cauldron Concrete Powder"			# Name of the datapack, used for messages and items lore
-MINECRAFT_VERSION: str = "1.21"				# Text used when loading the datapack to warn the user when the data version is not right
-VERSION: str = "1.1.0"						# Datapack version in the following mandatory format: major.minor.patch, ex: 1.0.0 or 1.21.615
+PROJECT_NAME: str = "Cauldron Concrete Powder"			# Name of the datapack, used for messages and items lore
+VERSION: str = "1.2.0"						# Datapack version in the following mandatory format: major.minor.patch, ex: 1.0.0 or 1.21.615
 NAMESPACE: str = "cauldron_concrete_powder"	# Should be the same you use in the merge folder. Used to namespace functions, tags, etc.
-DATAPACK_FORMAT: int = 48					# Pack format version, see https://minecraft.wiki/w/Pack_format#List_of_data_pack_formats
-DESCRIPTION = f"{DATAPACK_NAME} [{VERSION}] by {AUTHOR}"	# Pack description displayed in pack.mcmeta
+DESCRIPTION = f"{PROJECT_NAME} [{VERSION}] by {AUTHOR}"	# Pack description displayed in pack.mcmeta
 
 
 # Configuration dictionnary
@@ -24,12 +23,11 @@ configuration = {
 
 	"build_folder": BUILD_FOLDER,
 	"assets_folder": ASSETS_FOLDER,
+	"build_copy_destinations": BUILD_COPY_DESTINATIONS,
 	"author": AUTHOR,
-	"datapack_name": DATAPACK_NAME,
-	"minecraft_version": MINECRAFT_VERSION,
+	"project_name": PROJECT_NAME,
 	"version": VERSION,
 	"namespace": NAMESPACE,
-	"datapack_format": DATAPACK_FORMAT,
 	"description": DESCRIPTION,
 }
 
---
+++ continuous_delivery/pmc_config.py
@@ -0,0 +1,13 @@
+
+# Configuration for PlanetMinecraft
+from config import *
+
+# Constants
+PMC_URL: str = "https://www.planetminecraft.com/account/manage/data-packs/5879471/"
+
+# Configuration
+pmc_config: dict = {
+	"project_url": PMC_URL,
+	"version": VERSION,
+}
+
